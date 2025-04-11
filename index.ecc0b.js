d48f89cfa307a125ada89ab10eccd0c6c9fc85d3
alesandrshy <alesandrshyti454@gmail.com>
2025-04-11 03:22:03 +0000

    deploy V0.2.1.1

--- index.ecc0b.js
+++ index.ecc0b.js
@@ -1,4 +1,4 @@
-System.register(["./application.72d04.js"], function (_export, _context) {
+System.register(["./application.8c16b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
