2715d20835b636a1c1887d001a73d20ff10823c7
alesandrshy <alesandrshyti454@gmail.com>
2025-05-16 08:58:43 +0000

    deploy V0.2.5.9

--- application.46af3.js
+++ application.46af3.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.e4ee5.json';
+          this.settingsPath = 'src/settings.d19f1.json';
           this.showFPS = false;
         }
         init(engine) {
--- index.25636.js
+++ index.25636.js
@@ -1,4 +1,4 @@
-System.register(["./application.c06e7.js"], function (_export, _context) {
+System.register(["./application.46af3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
